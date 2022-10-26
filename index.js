17d531af9a5478fa88f0c8d932ed93a846854ed6
Pranto das <prantochandrodas@gmail.com>
2022-10-26 23:05:12 +0600

    add categories

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const app = express();
 const cors=require('cors')
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json');
+const posts=require('./data/Post.json');
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
 app.get('/category',(req,res)=>{
     res.send(categories);
+});
+app.get('/category/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedCourse=posts.find(post=>post.category_id===id);
+    res.send(selectedCourse);
 })
+
 
 app.listen(port, () => {
     console.log('Learn development Server running on port', port);
-})
+});
